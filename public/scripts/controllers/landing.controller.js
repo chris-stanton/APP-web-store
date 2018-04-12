a906ece1918c296d3555c134c50b0cd6174785f0
Chris Stanton <cstanton0760@yahoo.com>
2018-04-11 19:04:14 -0500

    added styling to landing, nav bar, footer, warranty and resources views

--- public/scripts/controllers/landing.controller.js
+++ public/scripts/controllers/landing.controller.js
@@ -1,12 +1,64 @@
 
 myApp.controller('LandingController',['EmailFactory', 'alertify', '$scope', function(EmailFactory, alertify, $scope) {
 
-  console.log("landing controller running...");
   const self = this;
 
   // defining alertify options
   alertify.delay(5000);
   alertify.logPosition('top right');
+
+  // gallary images
+  self.gallaryInfo = [
+    {
+      image: './assets/images/gallary/1.jpg',
+      tooltip: 'Three thread tiger wrap, 5" cork grip'
+    },
+    {
+      image: './assets/images/gallary/2.jpg',
+      tooltip: 'Three thread tiger wrap, split cork grip'
+    },
+    {
+      image: './assets/images/gallary/3.jpg',
+      tooltip: 'Four thread tiger, 6`6" RodGeeks C266MLXF'
+    },
+    {
+      image: './assets/images/gallary/4.jpg',
+      tooltip: 'Fully closed off wrap, 5" 3D printed grip'
+    },
+    {
+      image: './assets/images/gallary/5.jpg',
+      tooltip: 'Three thread tiger wrap, White camo hydrodipped noodle blank, 3" cork grip'
+    },
+    {
+      image: './assets/images/gallary/6.jpg',
+      tooltip: 'Three thread tiger, 6`6" RodGeeks C266MLXF'
+    },
+    {
+      image: './assets/images/gallary/7.jpg',
+      tooltip: 'Three thread tiger wrap, Orange camo hydrodipped noodle blank, 3" cork grip'
+    },
+    {
+      image: './assets/images/gallary/8.jpg',
+      tooltip: 'Razor pink spiral wrap'
+    },
+    {
+      image: './assets/images/gallary/9.jpg',
+      tooltip: 'Green Bay Packers razor spiral wrap'
+    },
+    {
+      image: './assets/images/gallary/10.jpg',
+      tooltip: 'Open water tiger wrapped rods'
+    },
+    {
+      image: './assets/images/gallary/11.jpg',
+      tooltip: '3 matching MN Vikings tiger wrapped noodle rods'
+    },
+    {
+      image: './assets/images/gallary/12.jpg',
+      tooltip: 'Four thread tiger with one off custom modlded fiberglass grip'
+    }
+  ];
+
 
 
 }]); // end myAPP.controller

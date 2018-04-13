0fd9d8f895c3742545888c273bdfe5ac578dd857
Chris Stanton <cstanton0760@yahoo.com>
2018-04-13 15:25:11 -0500

    added styling to parts and shop html files

--- public/scripts/controllers/shop.controller.js
+++ public/scripts/controllers/shop.controller.js
@@ -7,18 +7,21 @@ myApp.controller('ShopController',['StoreFactory', 'alertify', '$scope', functio
   self.items = [
     {
       id: 0,
-      item: "Custom Rods",
-      img: "custom-builds.png"
+      item: "Custom Build Rods",
+      img: "custom-builds.png",
+      link: "#!custom_builds"
     },
     {
       id: 1,
-      item: "Standard Rods",
-      img: "standard-builds.png"
+      item: "Standard Build Rods",
+      img: "standard-builds.png",
+      link: "#!standard_builds"
     },
     {
       id: 2,
       item: "Parts & Supplies",
-      img: "blanks.png"
+      img: "blanks.png",
+      link: "#!parts"
     }
   ];
 

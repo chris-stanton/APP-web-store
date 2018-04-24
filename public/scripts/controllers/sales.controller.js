0479ae808728d0303bd03cebcc7bb6fbe6ff3c65
Chris Stanton <cstanton0760@yahoo.com>
2018-04-24 11:29:51 -0500

    added to sales styling

--- public/scripts/controllers/sales.controller.js
+++ public/scripts/controllers/sales.controller.js
@@ -10,15 +10,16 @@ myApp.controller('SalesController',['StoreFactory', 'alertify', '$scope', functi
       blank_length: '28',
       guides: 'Standard light weight fly guides',
       handle: '5" Tapered Cork',
-      guide_wraps: 'Blank matching',
-      other_wrap: 'Purple, neon yellow, white tiger wrap',
+      wraps: 'Blank matching',
       price: '150',
       sale_price:'120',
+      sale_active: true,
       img_0: 'vikings/1.png',
       img_1: 'vikings/2.png',
       img_2: 'vikings/3.png',
       img_3: 'vikings/4.png',
-      notes: 'mn vikings'
+      notes: 'mn vikings',
+      active: true
     },
     {
       id: 1,
@@ -26,17 +27,23 @@ myApp.controller('SalesController',['StoreFactory', 'alertify', '$scope', functi
       blank_length: '32',
       guides: 'Standard light weight fly guides',
       handle: '5" Tapered Cork',
-      guide_wraps: 'Glow green',
-      other_wrap: 'Kiwi, dark blue, red tiger wrap',
+      wraps: 'Glow green',
       price: '150',
       sale_price:'120',
+      sale_active: true,
       img_0: 'walleye/1.png',
       img_1: 'walleye/2.png',
       img_2: 'walleye/3.png',
       img_3: 'walleye/4.png',
-      notes: 'medium heavy walleye'
+      notes: 'medium heavy walleye',
+      active: true
     }
   ];
+
+  // button click listener
+  self.buy = (item_id) => {
+    console.log(item_id);
+  };
 
 
 

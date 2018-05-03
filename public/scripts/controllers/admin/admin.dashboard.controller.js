f310d289a9227f0e221d19ab91d3944d8d3a36fc
Chris Stanton <cstanton0760@yahoo.com>
2018-05-03 10:34:18 -0500

    removed contact html directions form view

--- public/scripts/controllers/admin/admin.dashboard.controller.js
+++ public/scripts/controllers/admin/admin.dashboard.controller.js
@@ -12,11 +12,13 @@ myApp.controller('AdmindashboardController',['EmailFactory', 'alertify', '$scope
 			     "<h5># Units sold: </td><td>"+(d.unitsSold)+"</h5>";
 	};
 	let testData = [];
-	["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+	[
+    "HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
 	"ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
 	"MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
 	"CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
-	"WI", "MO", "AR", "OK", "KS", "LS", "VA"]
+	"WI", "MO", "AR", "OK", "KS", "LS", "VA"
+  ]
 		.forEach(function(d){
       // data per dataset
 			let totalSalesAmount = Math.round(100*Math.random());
@@ -32,7 +34,7 @@ myApp.controller('AdmindashboardController',['EmailFactory', 'alertify', '$scope
 	/* draw states on #statesvg */
 	uStates.draw("#statesvg", testData, tooltip);
 
-  
+
 
 
 

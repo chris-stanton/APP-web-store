8c4b6fb552397c33e54607e3e400f2717c0e36f2
Chris Stanton <cstanton0760@yahoo.com>
2018-06-28 23:05:44 -0500

    added more styling

--- public/scripts/controllers/admin/admin.gallaryImage.controller.js
+++ public/scripts/controllers/admin/admin.gallaryImage.controller.js
@@ -10,23 +10,40 @@ myApp.controller('AdmingallaryimageController',['GallaryFactory', 'alertify', '$
 
   // gets specific image by ID
   GallaryFactory.getImgDetails(image_id);
-  // return of specific imge details from DB
+  // return of specific image details from DB
   self.imageDetails = GallaryFactory.imageDetails;
 
-  // updates image details
+  // updates image
+  self.updateImage = () => {
+    console.log("update image button click");
+    // self.buttonStatus = '';
+    alertify.confirm('Select OK to update image',
+      function(){
+        // needs to be fired with factory response
+        // remain in the same view
+        alertify.success('Image Updated');
+      },
+      function(){
+        alertify.log('Updating image has been cancelled');
+      }
+    );
+  }; // end updateImage()
+
+
+  // updates ALL image and details
   self.updateGallaryImage = (imageDetail, buttonStatus) => {
     console.log(imageDetail);
     // self.buttonStatus = '';
-    alertify.confirm('Select OK to update image',
+    alertify.confirm('Select OK to update image and tooltip',
       function(imageDetail){
         self.buttonStatus = "inactive";
         // needs to be fired with factory response
-        alertify.success('Image Updated');
         // $location.path('/admin_gallary');
+        alertify.success('Image and tooltip Updated');
       },
       function(imageDetail){
         self.buttonStatus = "active";
-        alertify.log('The submission process has been cancelled');
+        alertify.log('Updating image and tooltip has  been cancelled');
       }
     );
   }; // end updateGallaryImage()

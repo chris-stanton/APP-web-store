374849f85e68c40f0eaf145304ce71a5145766e1
Chris Stanton <cstanton0760@yahoo.com>
2018-07-14 16:39:40 -0500

    working admin inventory js and html ]file

---
+++ public/scripts/controllers/admin/admin.inventory.controller.js
@@ -0,0 +1,10 @@
+
+myApp.controller('AdmininventoryController',['alertify', '$scope', '$routeParams', '$location', function(alertify, $scope, $routeParams, $location) {
+
+  const self = this;
+  self.buttonStatus = 'active';
+
+  
+
+
+}]); // end myAPP.controller
